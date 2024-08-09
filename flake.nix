09fa6fad95be1238d8c01b633fceeaf8088ff788
Alexandre Negrel <alexandre@negrel.dev>
2024-08-09 10:39:07 +0200

    add nix package

--- flake.nix
+++ flake.nix
@@ -16,6 +16,22 @@
             buildInputs = with pkgs; [ nodejs_18 ];
           };
         };
+        packages = {
+          default = pkgs.buildNpmPackage {
+            pname = "gis";
+            version = "0.4.0";
+
+            src = ./.;
+
+            npmDepsHash = "sha256-joOSbzqtAJJ8MNWU93hm2qHPM/p4vzj+cdEJ1c7IOeE=";
+
+            meta = {
+              description = "Script to get your site indexed on Google in less than 48 hours.";
+              homepage = "https://github.com/goenning/google-indexing-script";
+              license = lib.licenses.mit;
+            };
+          };
+        };
       }
     );
 }
